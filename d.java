8369c48d1b5e999c69ca66a9a0cee759b40249c8
polurumanasa3 <manasap142@gmail.com>
2024-12-04 22:54:02 -0800

    Add files via upload

---
+++ d.java
@@ -0,0 +1,27 @@
+import java .util.*;
+class d{
+    public static void main(String args[])
+    {
+    Scanner Sc=new Scanner(System.in);
+    for (int i=0;i<5;i++)
+    {
+     int n=Sc.nextInt();
+     if((n&7)==0)
+     {
+      System.out.println("multiple of 8");
+     }
+    else
+     {
+    System.out.println("not multiple of 8");
+     }
+    }
+    }
+}
+
+            
+            
+        
+        
+    
+
+      
