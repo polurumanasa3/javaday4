c59e6d3ec6833232efc1921f5a43e38bcaee67e1
polurumanasa3 <manasap142@gmail.com>
2024-12-04 21:18:07 -0800

    Add files via upload

---
+++ b.java
@@ -0,0 +1,43 @@
+import java.util.*;
+class b
+{
+public static void main(String args[])
+{
+Scanner Sc=new Scanner(System.in);
+ int day=Sc.nextInt();
+switch(day)
+{
+    case 1:
+        System.out.println("monday");
+        break;
+    case 2:
+        System.out.println("tuesday");
+        break;
+    case 3:
+        System.out.println("wedsday");
+        break;
+    case 4:
+        System.out.println("thusday");
+        break;
+    case 5:
+        System.out.println("friday");
+        break;
+    case 6:
+        System.out.println("saturday");
+        break;
+    case 7:
+        System.out.println("sunday");
+        break;
+    default:
+        System.out.println("invalid");
+        break;
+}
+}
+}
+
+
+
+
+
+
+
