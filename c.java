09e362c40df78ce3c61de2a47b329592f11784bb
polurumanasa3 <manasap142@gmail.com>
2024-12-04 22:19:32 -0800

    Add files via upload

---
+++ c.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class c
+{
+public static void main(String args[])
+{
+Scanner Sc=new Scanner(System.in);
+int a=Sc.nextInt();
+int b=Sc.nextInt();
+ System.out.println((a<b)?"a is smaller":"b is smaller");
+
+}
+}
