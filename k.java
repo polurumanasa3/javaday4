f6e5d7d6b2cac727f68ab8bdf483c9620ab79a08
polurumanasa3 <manasap142@gmail.com>
2024-12-05 02:21:10 -0800

    Add files via upload

---
+++ k.java
@@ -0,0 +1,31 @@
+import java.util.*;
+class k
+{
+public static void main(String args[])
+{
+ Scanner Sc=new Scanner(System.in);
+int n=Sc.nextInt();
+int c=0;
+for(int i=1;i*i<=n;i++)
+{
+if(n%i==0)
+ {
+ c+=1;
+if(i*i!=n)
+{
+ c+=1;
+ }
+ }
+}
+if(c==2)
+{
+System.out.println("prime");
+}
+else
+{
+System.out.println("not prime");
+}
+}
+}
+
+
