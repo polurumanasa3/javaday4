a539aa81d95402ccc6e43391e0044cab00ea3a4d
polurumanasa3 <manasap142@gmail.com>
2024-12-05 01:20:19 -0800

    Add files via upload

---
+++ h.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class h
+{
+public static void main(String args[])
+{
+ Scanner Sc=new Scanner(System.in);
+int t=Sc.nextInt();
+for(int i=1;i<25;i+=2)
+{
+    System.out.println(i);
+}
+}
+}
+
+
